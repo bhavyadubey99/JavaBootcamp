49e4d37791250b27c23f543e6dc2420997cd7c64
bhavyadubey99 <bhavyadubey99@gmail.com>
2021-06-02 01:35:37 +0530

    Hashmaps - 3(Rehashing & more)

--- src/HashMapImplementation/HashMap.java
+++ src/HashMapImplementation/HashMap.java
@@ -58,13 +58,144 @@ public class HashMap {
 		// key was not already present.
 		Node newNode = new Node(key, value);
 
-		Node head = bucketArray[bucketNo];
+		// Node head = bucketArray[bucketNo];
 
 		// adding node to the first.
-		newNode.next = head;
-		head = newNode;
+		newNode.next = bucketArray[bucketNo];
+		bucketArray[bucketNo] = newNode;
 		size++;
+
+		// calculating load factor
+		double loadFactor = (1.0 * size) / bucketArray.length;
+
+		int threshold = 2;
+
+		if (loadFactor > threshold) {
+			reHash();
+		}
 
 	}
 
+	public Integer get(String key) {
+
+		int bucketNo = hashFunction(key);
+
+		Node temp = bucketArray[bucketNo];
+
+		while (temp != null) {
+
+			if (temp.key.equals(key)) {
+				return temp.value;
+			}
+
+			temp = temp.next;
+		}
+
+		return null;
+	}
+
+	public boolean containsKey(String key) {
+
+		int bucketNo = hashFunction(key);
+
+		Node temp = bucketArray[bucketNo];
+
+		while (temp != null) {
+
+			if (temp.key.equals(key)) {
+				return true;
+			}
+			temp = temp.next;
+		}
+
+		return false;
+	}
+
+	public Integer remove(String key) {
+
+		// getting the bucketNo as usual
+		int bucketNo = hashFunction(key);
+
+		// checking whether the key even exists?
+		Node temp = bucketArray[bucketNo];
+		Node prev = null;
+
+		while (temp != null) {
+
+			if (temp.key.equals(key)) {
+				break;
+			}
+
+			prev = temp;
+			temp = temp.next;
+		}
+
+		// key was not present if flow of code comes here.
+		if (temp == null) {
+			return null;
+		}
+
+		// if we come out loop due to break keyword.
+		if (temp != bucketArray[bucketNo]) {
+			// it is not a head node.
+			prev.next = temp.next;
+
+		} else {
+			// the node is a head node.
+			bucketArray[bucketNo] = temp.next;
+		}
+
+		size--;
+		return temp.value;
+
+	}
+
+	@Override
+	public String toString() {
+
+		String res = "";
+		for (Node temp : bucketArray) {
+
+			while (temp != null) {
+
+				res += temp.key + " = " + temp.value + "  ";
+
+				temp = temp.next;
+			}
+
+		}
+
+		return res;
+	}
+
+	private void reHash() {
+
+		Node[] oldBucketArray = bucketArray;
+
+		// creating new bucketarray twice the size
+		Node[] newBucketArray = new Node[oldBucketArray.length * 2];
+
+		// changing the bucket array of the hashmap.
+		this.bucketArray = newBucketArray;
+
+		// new size - reset, as new array has no elements.
+		size = 0;
+
+		// copying and arranging elements of old bucket array to the new bucket array.
+
+		for (Node temp : oldBucketArray) {
+			// going through elements on old one.
+
+			while (temp != null) {
+
+				// placing it in new bucket array
+				put(temp.key, temp.value);
+
+				// onto next element.
+				temp = temp.next;
+
+			}
+
+		}
+	}
 }
--- src/HashMapImplementation/HashMapClient.java
+++ src/HashMapImplementation/HashMapClient.java
@@ -4,7 +4,28 @@ public class HashMapClient {
 
 	public static void main(String[] args) {
 
-		new HashMap();
+		HashMap map = new HashMap();
+		
+		map.put("A", 10);
+		map.put("B", 30);
+		map.put("C", 20);
+		map.put("D", 60);
+		map.put("E", 80);
+		
+		System.out.println(map);
+		
+		System.out.println(map.remove("B"));
+		
+		System.out.println(map);
+		
+		System.out.println(map.get("D"));
+		System.out.println(map.get("G"));
+
+		System.out.println(map.containsKey("E"));
+
+
+		
+		
 	}
 
 }
