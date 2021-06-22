496ef5b168f658ed5cd91aeb99b7d068cc56afba
bhavyadubey99 <bhavyadubey99@gmail.com>
2021-06-22 20:08:01 +0530

    Sanket And Strings

---
+++ src/practiceQuestionsExtra/sanketAndStrings.java
@@ -0,0 +1,30 @@
+package practiceQuestionsExtra;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class sanketAndStrings {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int k = sc.nextInt();
+
+		String s = sc.next();
+
+		System.out.println(kchange(s, k));
+
+	}
+
+	public static int kchange(String s, int k) {
+		
+		int[] count = new int[2]; //0 -> A , 1 -> B
+		
+
+		
+		return 0;
+
+	}
+
+}
